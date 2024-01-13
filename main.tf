ba6ab85b2b07cc5af4440fa4ac3a4b1aa2ee4185
Stanley Stuart <stanley.stuart@hashicorp.com>
2024-01-12 18:40:00 -0800

    test

--- main.tf
+++ main.tf
@@ -4,5 +4,13 @@
       source  = "hashicorp/random"
       version = "~> 3.1.0"
     }
+    null = {
+      source = "hashicorp/null"
+      version = "~> 3.2.2"
+    }
   }
+}
+
+resource "null_resource" "pet" {
+  for_each = range(1, 500)
 }
